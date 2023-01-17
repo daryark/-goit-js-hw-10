28f6e2bfd3e66eaee2299f0c0489cdf51c2e2228
Dasha Yarkovska <yarkovska11@gmail.com>
2023-01-18 01:34:01 +0200

    task done

--- src/index.js
+++ src/index.js
@@ -1,19 +1,77 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+
 import { fetchCountries } from './js/fetch-countries';
-import { renderCardMarkup } from './js/country-card';
+// import { checkResults } from './js/check-results';
 
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
+const countryCard = document.querySelector('.country-info');
+const countriesList = document.querySelector('.country-list');
 
 inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(e) {
+  clearContent();
   const name = e.target.value.trim();
 
-  fetchCountries(name)
-    .then(renderCardMarkup)
-    // .then(r => console.log(r))
-    .catch(err => console.log(err));
+  fetchCountries(name).then(checkResults).catch(errMessage);
+}
+
+function checkResults(res) {
+  if (res.length > 10) {
+    clearContent();
+    infoMessage();
+  } else if (res.length <= 10 && res.length > 1) {
+    renderCountriesMarkup(res);
+  } else if (res.length === 1) {
+    renderCardMarkup(res);
+  }
+}
+
+function createCardMarkup(country) {
+  const {
+    name: { official },
+    capital,
+    population,
+    flags: { svg },
+    languages,
+  } = country[0];
+  return `<img src="${svg}" alt="${official} flag">
+  <h1>${official}</h1>
+  <p><span>Capital : </span>${capital}</p>
+  <p><span>Population : </span>${population}</p>
+  <p><span>Languages : </span>${Object.values(languages).join(', ')}</p>`;
+}
+
+function renderCardMarkup(markup) {
+  countryCard.innerHTML = createCardMarkup(markup);
+}
+
+function createCountriesMarkup(countries) {
+  return countries
+    .map(
+      ({ name: { official }, flags: { svg } }) =>
+        `<li><img src = "${svg}" alt = "${official} flag">${official}</li>`
+    )
+    .join('');
+}
+
+function renderCountriesMarkup(markup) {
+  countriesList.innerHTML = createCountriesMarkup(markup);
+}
+
+function infoMessage() {
+  Notify.info('Too many matches found. Please enter a more specific name.');
+}
+
+function errMessage(err) {
+  Notify.failure('Oops, there is no country with that name');
+}
+
+function clearContent() {
+  countriesList.innerHTML = '';
+  countryCard.innerHTML = '';
 }
--- src/js/fetch-countries.js
+++ src/js/fetch-countries.js
@@ -2,4 +2,9 @@ const BASE_URL = 'https://restcountries.com/v3.1';
 const params = 'fields=name,capital,population,languages,flags';
 
 export const fetchCountries = name =>
-  fetch(`${BASE_URL}/name/${name}?${params}`).then(r => r.json());
+  fetch(`${BASE_URL}/name/${name}?${params}`).then(r => {
+    if (!r.ok) {
+      throw new Error(r.status);
+    }
+    return r.json();
+  });
